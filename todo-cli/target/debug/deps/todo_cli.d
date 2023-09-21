e5345cee481e9ae9f064105e77e0d5c3ede34d04
lppcse <lokeshcse1990@gmail.com>
2023-09-21 18:13:50 -0500

    Rust hello wold ! via cargo

---
+++ todo-cli/target/debug/deps/todo_cli.d
@@ -0,0 +1,5 @@
+E:\Rust_Setup_Via_Cargo\Rust_SetUp_Via_Cargo\todo-cli\target\debug\deps\todo_cli.exe: src\main.rs
+
+E:\Rust_Setup_Via_Cargo\Rust_SetUp_Via_Cargo\todo-cli\target\debug\deps\todo_cli.d: src\main.rs
+
+src\main.rs:
